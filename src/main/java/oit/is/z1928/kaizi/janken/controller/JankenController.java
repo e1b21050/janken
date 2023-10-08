92f1e6573eab76310bc82b9d1ceb8ec5a3498eff
koichi-shirakashi <e1b21050@oit.ac.jp>
2023-10-08 22:39:59 +0900

    cpu-flowの達成

--- src/main/java/oit/is/z1928/kaizi/janken/controller/JankenController.java
+++ src/main/java/oit/is/z1928/kaizi/janken/controller/JankenController.java
@@ -1,16 +1,60 @@
 package oit.is.z1928.kaizi.janken.controller;
 
+import oit.is.z1928.kaizi.janken.model.Janken;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class JankenController {
+  private Janken janken = new Janken();
+  private int cnt_game = 0;
+  private int cnt_win = 0;
+  private int cnt_draw = 0;
+  private int cnt_lose = 0;
+
+  @GetMapping("/index")
+  public String indexPage() {
+    return "janken";
+  }
+
+  @PostMapping("/janken")
+  public String JankenPage(@RequestParam("username") String username, ModelMap model) {
+    janken.setUserName(username);
+    model.addAttribute("username", janken.getUserName());
+    return "janken";
+  }
 
   @GetMapping("/janken")
-  public String jankenPage(@RequestParam("username") String username, Model model) {
-    model.addAttribute("username", username);
+  public String jankenPage() {
+    return "janken";
+  }
+
+  @GetMapping("/jankengame")
+  public String jankengamePage(@RequestParam("choice") String choice,
+      ModelMap model) {
+    janken.setUserChoice(choice);
+    String cpuChoice = janken.randomHand();
+    janken.setCpuChoice(cpuChoice);
+    String result = janken.detResult(janken.getUserChoice(), janken.getCpuChoice());
+    cnt_game++;
+    if (result.equals("You Win!")) {
+      cnt_win++;
+    } else if (result.equals("You Lose!")) {
+      cnt_lose++;
+    } else if (result.equals("Draw!")) {
+      cnt_draw++;
+    }
+    model.addAttribute("choice", janken.getUserChoice());
+    model.addAttribute("cpu_choice", janken.getCpuChoice());
+    model.addAttribute("result", result);
+    model.addAttribute("cnt_game", cnt_game);
+    model.addAttribute("cnt_win", cnt_win);
+    model.addAttribute("cnt_lose", cnt_lose);
+    model.addAttribute("cnt_draw", cnt_draw);
+
     return "janken";
   }
 }
--- src/main/java/oit/is/z1928/kaizi/janken/model/Janken.java
+++ src/main/java/oit/is/z1928/kaizi/janken/model/Janken.java
@@ -1,17 +1,56 @@
 package oit.is.z1928.kaizi.janken.model;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import java.util.Random;
 
-import ch.qos.logback.core.model.Model;
-
-@Controller
 public class Janken {
+  private String userName;
+  private String userChoice;
+  private String cpuChoice;
 
-  @GetMapping("/my-page")
-  public String myPage(Model model) {
-
-    // ビューの名前を返す
-    return "my-page";
+  public String getUserName() {
+    return this.userName;
   }
+
+  public void setUserName(String userName) {
+    this.userName = userName;
+  }
+
+  public String getUserChoice() {
+    return this.userChoice;
+  }
+
+  public void setUserChoice(String userChoice) {
+    this.userChoice = userChoice;
+  }
+
+  public String getCpuChoice() {
+    return this.cpuChoice;
+  }
+
+  public void setCpuChoice(String cpuChoice) {
+    this.cpuChoice = cpuChoice;
+  }
+
+  public String randomHand() {
+    Random random = new Random();
+    int num = random.nextInt(3);
+    if (num == 0) {
+      return "Gu";
+    } else if (num == 1) {
+      return "Ch";
+    } else {
+      return "Pa";
+    }
+  }
+
+  public String detResult(String userChoice, String cpuChoice) {
+    if ((userChoice.equals("Pa") && cpuChoice.equals("Gu")) || (userChoice.equals("Gu") && cpuChoice.equals("Ch"))
+        || (userChoice.equals("Ch") && cpuChoice.equals("Pa"))) {
+      return "You Win!";
+    } else if (userChoice.equals(cpuChoice)) {
+      return "Draw!";
+    }
+    return "You Lose!";
+  }
+
 }
