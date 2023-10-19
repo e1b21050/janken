09add7c2d6f71a232df9656150ef76952e1ce3b0
koichi-shirakashi <e1b21050@oit.ac.jp>
2023-10-19 20:31:16 +0900

    ユーザ情報をSELECTで取得して表示

--- src/main/java/oit/is/z1928/kaizi/janken/controller/JankenController.java
+++ src/main/java/oit/is/z1928/kaizi/janken/controller/JankenController.java
@@ -1,18 +1,21 @@
 package oit.is.z1928.kaizi.janken.controller;
 
 import java.security.Principal;
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.beans.factory.annotation.Autowired;
-import oit.is.z1928.kaizi.janken.model.Entry;
+import oit.is.z1928.kaizi.janken.model.User;
+import oit.is.z1928.kaizi.janken.model.UserMapper;
 import oit.is.z1928.kaizi.janken.model.Janken;
 
 @Controller
 public class JankenController {
   @Autowired
-  private Entry entry;
+  UserMapper userMapper;
 
   private Janken janken = new Janken();
 
@@ -25,22 +28,25 @@ public class JankenController {
   @GetMapping("/janken")
   public String JankenPage(ModelMap model, Principal prin) {
     String loginUser = prin.getName();
-    this.entry.addUser(loginUser);
-    model.addAttribute("entry", this.entry);
     model.addAttribute("username", loginUser);
+    ArrayList<User> users = userMapper.selectAllByUsersName();
+    model.addAttribute("users", users);
     return "janken";
   }
 
-  @GetMapping("/jankengame")
-  public String jankengamePage(@RequestParam("choice") String choice,
-      ModelMap model) {
-    janken.setUserChoice(choice);
-    String cpuChoice = janken.randomHand();
-    janken.setCpuChoice(cpuChoice);
-    String result = janken.detResult(janken.getUserChoice(), janken.getCpuChoice());
-    model.addAttribute("choice", janken.getUserChoice());
-    model.addAttribute("cpu_choice", janken.getCpuChoice());
-    model.addAttribute("result", result);
-    return "janken";
-  }
+  /*
+   * @GetMapping("/jankengame")
+   * public String jankengamePage(@RequestParam("choice") String choice,
+   * ModelMap model) {
+   * janken.setUserChoice(choice);
+   * String cpuChoice = janken.randomHand();
+   * janken.setCpuChoice(cpuChoice);
+   * String result = janken.detResult(janken.getUserChoice(),
+   * janken.getCpuChoice());
+   * model.addAttribute("choice", janken.getUserChoice());
+   * model.addAttribute("cpu_choice", janken.getCpuChoice());
+   * model.addAttribute("result", result);
+   * return "janken";
+   * }
+   */
 }
