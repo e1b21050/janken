766b94fe684b0d680fb496df3427101303003813
koichi-shirakashi <e1b21050@oit.ac.jp>
2023-10-19 14:18:20 +0900

    DBの初期化処理を実装

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,12 @@
+CREATE TABLE users (
+    id IDENTITY,
+    name VARCHAR NOT NULL
+);
+
+CREATE TABLE matches (
+    id IDENTITY,
+    user1 INTEGER NOT NULL,
+    user2 INTEGER NOT NULL,
+    user1Hand VARCHAR NOT NULL,
+    user2Hand VARCHAR NOT NULL
+);
