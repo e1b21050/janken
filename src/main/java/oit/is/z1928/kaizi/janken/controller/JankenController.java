73c05754aa81f195be0268ffe2aba774e57b6bc4
koichi-shirakashi <e1b21050@oit.ac.jp>
2023-10-30 15:19:40 +0900

    対戦相手が手を選び結果を表示

--- src/main/java/oit/is/z1928/kaizi/janken/controller/JankenController.java
+++ src/main/java/oit/is/z1928/kaizi/janken/controller/JankenController.java
@@ -7,15 +7,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 import oit.is.z1928.kaizi.janken.model.User;
 import oit.is.z1928.kaizi.janken.model.UserMapper;
 import oit.is.z1928.kaizi.janken.model.Match;
 import oit.is.z1928.kaizi.janken.model.MatchInfo;
 import oit.is.z1928.kaizi.janken.model.MatchInfoMapper;
 import oit.is.z1928.kaizi.janken.model.MatchMapper;
-import oit.is.z1928.kaizi.janken.model.Janken;
+//import oit.is.z1928.kaizi.janken.model.Janken;
+import oit.is.z1928.kaizi.janken.service.AsyncKekka;
 
 @Controller
 public class JankenController {
@@ -28,13 +31,14 @@ public class JankenController {
   @Autowired
   MatchInfoMapper matchInfoMapper;
 
-  private Janken janken = new Janken();
+  @Autowired
+  AsyncKekka asyncKekka;
+
+  // private Janken janken = new Janken();
 
   @GetMapping("/janken")
   @Transactional
-  public String JankenPage(ModelMap model, Principal prin) {
-    String loginUser = prin.getName();
-    model.addAttribute("username", loginUser);
+  public String JankenPage(ModelMap model) {
     ArrayList<User> users = userMapper.selectAllByUsersName();
     model.addAttribute("users", users);
     ArrayList<MatchInfo> matchinfos = matchInfoMapper.selectByMatchInfo();
@@ -45,9 +49,7 @@ public class JankenController {
   }
 
   @GetMapping("/match")
-  public String MatchPage(@RequestParam Integer id, ModelMap model, Principal prin) {
-    String loginUser = prin.getName();
-    model.addAttribute("username", loginUser);
+  public String MatchPage(@RequestParam Integer id, ModelMap model) {
     User users = userMapper.selectById(id);
     model.addAttribute("users", users);
     return "match";
@@ -56,30 +58,38 @@ public class JankenController {
   @GetMapping("/fight")
   @Transactional
   public String FightPage(@RequestParam Integer id, @RequestParam String hand, ModelMap model, Principal prin) {
-    User users = userMapper.selectById(id);
-    model.addAttribute("users", users);
     String loginUser = prin.getName();
     User user = userMapper.selectByName(loginUser);
-    model.addAttribute("username", loginUser);
-
-    /*
-     * Match match = new Match();
-     * String cpuHand = janken.randomHand();
-     * match.setUser1(user.getId());
-     * match.setUser2(id);
-     * match.setUser1Hand(hand);
-     * match.setUser2Hand(cpuHand);
-     * match.setActive(true);
-     * matchMapper.insertMatch(match);
-     * model.addAttribute("match", match);
-     */
 
     MatchInfo matchinfo = new MatchInfo();
-    matchinfo.setUser1(user.getId());
-    matchinfo.setUser2(id);
-    matchinfo.setUser1Hand(hand);
-    matchInfoMapper.insertMatchInfo(matchinfo);
-    model.addAttribute("matchinfo", matchinfo);
+    Match match = new Match();
+    if (matchInfoMapper.selectById(user.getId()) == null) {
+      matchinfo.setUser1(user.getId());
+      matchinfo.setUser2(id);
+      matchinfo.setUser1Hand(hand);
+      matchInfoMapper.insertMatchInfo(matchinfo);
+      model.addAttribute("matchinfo", matchinfo);
+    } else {
+      match.setUser1(user.getId());
+      match.setUser2(id);
+      match.setUser1Hand(hand);
+      match.setUser2Hand(matchInfoMapper.selectByUser2(user.getId()).getUser1Hand());
+      matchMapper.insertMatch(match);
+      final Match match2 = asyncKekka.syncShowMatch();
+      model.addAttribute("match", match2);
+      fight();
+      matchMapper.updateMatch(match);
+      matchInfoMapper.updateMatchInfo(matchinfo);
+    }
+
     return "wait";
   }
+
+  @PostMapping("/fight")
+  public SseEmitter fight() {
+    SseEmitter emitter = new SseEmitter();
+    asyncKekka.asyncShowKekka(emitter);
+    return emitter;
+  }
+
 }
--- src/main/java/oit/is/z1928/kaizi/janken/model/MatchInfoMapper.java
+++ src/main/java/oit/is/z1928/kaizi/janken/model/MatchInfoMapper.java
@@ -6,6 +6,7 @@ import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 @Mapper
 public interface MatchInfoMapper {
@@ -13,8 +14,17 @@ public interface MatchInfoMapper {
   @Select("SELECT * FROM matchinfo WHERE isActive = true;")
   ArrayList<MatchInfo> selectByMatchInfo();
 
+  @Select("SELECT * FROM matchinfo WHERE isActive=true and id = #{id}")
+  MatchInfo selectById(int id);
+
+  @Select("SELECT * FROM matchinfo WHERE isActive=true and user2 = #{id}")
+  MatchInfo selectByUser2(int id);
+
   @Insert("INSERT INTO matchinfo(user1,user2,user1Hand,isActive) VALUES (#{user1},#{user2},#{user1Hand},true);")
   @Options(useGeneratedKeys = true, keyColumn = "id", keyProperty = "id")
   void insertMatchInfo(MatchInfo matchInfo);
+
+  @Update("UPDATE matchinfo SET isActive = false")
+  void updateMatchInfo(MatchInfo matchInfo);
 
 }
